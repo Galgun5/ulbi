eef0be977b8cb49c6c4ce79a6be1b4090fbbe8e3
Artiom Hotinec <artiom.hotinec.work@gmail.com>
2023-10-01 19:33:26 +0300

    add jest, add classNames test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,12 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
         'airbnb',
-        'plugin:i18next/recommended'
+        'plugin:i18next/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -31,13 +32,17 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'no-unused-vars': 'warn',
         'react/function-component-definition': 'off',
-        'react/jsx-filename-extension': [2, {extensions: ['.js', '.jsx', '.tsx']}],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         'react/jsx-props-no-spreading': 'warn',
         'react/react-in-jsx-scope': 'off',
         'react/require-default-props': 'off',
-        'i18next/no-literal-string': ['error', {markupOnly: true}],
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
