2c881c67aa12bff49e34d80a4b061310644003fc
Artiom Hotinec <artiom.hotinec.work@gmail.com>
2023-11-30 21:08:06 +0200

    add Login module

--- src/widgets/NavBar/ui/NavBar.stories.tsx
+++ src/widgets/NavBar/ui/NavBar.stories.tsx
@@ -1,11 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { NavBar } from './NavBar';
 
 const meta = {
     title: 'widgets/NavBar',
     component: NavBar,
+    decorators: [StoreDecorator({})],
 } satisfies Meta<typeof NavBar>;
 
 export default meta;
@@ -14,4 +16,11 @@ type Story = StoryObj<typeof meta>;
 export const Light: Story = {};
 export const Dark: Story = {
     decorators: [ThemeDecorator(Theme.DARK)],
+};
+export const AuthNavBar: Story = {
+    decorators: [StoreDecorator({
+        user: {
+            authData: {},
+        },
+    })],
 };
